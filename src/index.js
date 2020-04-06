2c7f05684c1afec82ed3d84f8077439e5001f9dc
Christopher Floyd <cfloyd252@gmail.com>
2020-04-05 22:23:31 -0700

    succesfully added images

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './style.css';
-import $ from 'jquery'
+import $ from 'jquery';
+import welcomeImg from './images/falloutsuit.jpg'
+import quizSnippit from './images/quiz-snip.png'
 
 function generateWelcomeView(){
   return (
     ` <h1>Welcome</h1>
       <h2 id="opening_statement">My name is Chris and I went to math camp.</h2>
-      <img id='welcome_image' src={require('src/images/falloutsuit.jpg')} alt="Chris sitting and wearing a suit from the game Fallout">`
-  )
+      <img id='welcome_image' src="${welcomeImg}" alt="Chris sitting and wearing a suit from the game Fallout">`
+  );
 }
 
 
@@ -28,14 +30,14 @@ function generateBioView(){
       that my boys are becoming more independent, I finally found the time and 
       opportunity to rekindle my passion for coding.
     </p>`
-  )
+  );
 }
 
 function generateProjectView(){
   return(
     `<h2>Projects</h2>
     <h3>Borderlands Quiz</h3>
-    <img id='quiz_snippit' src="src/images/quiz-snip.PNG" alt="A snippet of the quiz application">
+    <img id='quiz_snippit' src="${quizSnippit}" alt="A snippet of the quiz application">
     <p>
       <span>Description:</span> An application that allows the user to play through a Borderlands 2 themed
       quiz.
@@ -45,29 +47,29 @@ function generateProjectView(){
       ||
       <a href="https://thinkful-ei-leopard.github.io/jorge-chris-quiz-app/" target="_blank">Live hosted page</a>
     </div>`
-  )
+  );
 }
 
 const STORE = {
-  sectionVal: `welcome`,
-}
+  sectionVal: 'welcome',
+};
 
 function render() {
-  if (STORE.sectionVal == `welcome`) {
+  if (STORE.sectionVal === 'welcome') {
     $('main').html(generateWelcomeView());
-  } else if (STORE.sectionVal == `bio`) {
+  } else if (STORE.sectionVal === 'bio') {
     $('main').html(generateBioView());
-  } else if (STORE.sectionVal == `projects`) {
+  } else if (STORE.sectionVal === 'projects') {
     $('main').html(generateProjectView());
   }
 }
 
 function handleSelectValue(){
-  let selectVal = $('select').change(() => {
-    STORE.sectionVal = $('select').val()
-    render()
-  })
+  $('select').change(() => {
+    STORE.sectionVal = $('select').val();
+    render();
+  });
 }
 
-render()
-handleSelectValue()
+render();
+handleSelectValue();
